3ae639281901a7b52e1bffc1f20855e909833862
SMwebdeveloper <samandarwebdeveloper@gamil.com>
2023-06-19 18:06:14 +0500

    getters

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,22 @@
         <h2>My Favorite Movies</h2>
       </header>
       <div class="tabs">
-        <button :class="['btn', { btn_green: movieStore.activeTab === 1 }]">
+        <button :class="['btn', { btn_green: movieStore.activeTab === 1 }]" @click="setup(1)">
           Favorite
         </button>
-        <button :class="['btn', { btn_green: movieStore.activeTab === 2 }]">
+        <button :class="['btn', { btn_green: movieStore.activeTab === 2 }]" @click="setup(2)">
           Search
         </button>
       </div>
       <div class="movies" v-if="movieStore.activeTab === 1">
-        <h3>All Movies</h3>
+        <div>
+        <h3>Watched Movies (count: {{ movieStore.watchedMovies.length }})</h3>
+        <Movie
+          v-for="movie of movieStore.watchedMovies"
+          :key="movie.id"
+          :movie="movie" />
+      </div>
+        <h3>All Movies (count: {{ movieStore.movies.length }})</h3>
         <Movie
           v-for="movie of movieStore.movies"
           :key="movie.id"
@@ -22,11 +29,14 @@
       <div class="search" v-else>Search</div>
     </main>
   </template>
-  
+
   <script setup>
   import Movie from "./components/Movie.vue";
   import { useMovieStore } from "./stores/MovieStore";
-  
+
+  const setup = (id) => {
+    movieStore.setActive(id)
+  }
   const movieStore = useMovieStore();
   </script>
 
--- src/stores/MovieStore.js
+++ src/stores/MovieStore.js
@@ -10,7 +10,7 @@ export const useMovieStore = defineStore("movieStore", {
           "After being bitten by a genetically altered spider at Oscorp, nerdy but endearing high school student Peter Parker is endowed with amazing powers to become the superhero known as Spider-Man.",
         poster_path: "/gh4cZbhZxyTbgxQPxD0dOudNPTn.jpg",
         release_date: "2002-05-01",
-        isWatched: false,
+        isWatched: true,
       },
       {
         id: 2,
@@ -24,4 +24,21 @@ export const useMovieStore = defineStore("movieStore", {
     ],
     activeTab: 1,
   }),
+  getters: {
+    watchedMovies() {
+      return this.movies.filter(el => el.isWatched)
+    }
+  },
+  actions: {
+    setActive(id) {
+      this.activeTab = id
+    },
+    toggleWatched(id) {
+      const idx = this.movies.findIndex((el) => el.id === id)
+      this.movies[idx].isWatched = !this.movies[idx].isWatched
+    },
+    deleteMovie(id) {
+      this.movies = this.movies.filter(el => el.id !== id)
+    }
+  }
 });
